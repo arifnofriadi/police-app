5ac25bae74a16b3d8e61645b0d6be9ccce4648ed
arifnofriadi <arifnofriadi230401@outlook.com>
2025-05-08 08:00:30 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['prefix' => 'panel-control', 'middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // vehicles routes
+    
+});
